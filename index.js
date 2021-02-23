f7c4a00cd6d52d977f7033bcd1448a39e6cab470
mm_sourcetree <iqm_dry@hotmail.co.jp>
2021-02-23 14:12:17 +0900

    初回コミット

---
+++ index.js
@@ -0,0 +1,26 @@
+const request = require("request");
+var fs = require("fs");
+
+
+function requestPromise() {
+    const param = {
+        url: "https://www.google.com",
+        method: "GET"
+    }
+    return new Promise((resolve, reject) => {
+        request(param, function(error, response, body) {
+            if (error) {
+                reject("ページを取得できませんでした");
+            } else {
+                console.log(body);
+                resolve(body);
+            }
+        })
+    })
+}
+
+(async function() {
+    res = await requestPromise();
+    fs.writeFileSync("response/output.html", res);
+    console.log('write end');
+})();
